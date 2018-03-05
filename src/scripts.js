0be9350712f8216c0b15b726a9e84b54c37992fa
Piotrek <piotr.michalski2000@gmail.com>
2018-03-06 00:05:15 +0100

    offer section almost ended + menu scroll effect

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,29 @@
 (function() {
- 
-    window.addEventListener( 'scroll', function() {
+    
+    // Menu scroll effect
+    window.addEventListener( 'scroll', () => {
+
+        const header = document.querySelector( '.header' ),
+              positionY = window.pageYOffset || document.documentElement.scrollTop;
+
+        if( window.innerWidth > 1023 ) {    
+
+            if( positionY > 250 ) {
+                header.classList.add( 'opacity' );
+            } else {
+                header.classList.remove( 'opacity' );
+            }
+        }
+
+    } );
+
+    // Second section container -> width++
+    window.addEventListener( 'scroll', () => {
+
         const section = document.querySelector( '.section__offer' );
-        let x = section.offsetWidth;
         const offset = window.pageYOffset;
         const max_width = 1250;
+        let x = section.offsetWidth;
 
         if( offset > 100 ) {
             section.style.width = x + 'px';
@@ -15,6 +34,7 @@
             section.style.width = max_width + 'px';
         } 
         console.log(x);
+
     } );
       
     
