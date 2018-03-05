52a61006f5f3b378bc076c95a373ce97271bbdd2
Piotrek <piotr.michalski2000@gmail.com>
2018-03-05 17:51:54 +0100

    header done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import { x } from './libs/lib';
 import './../scss/_style.scss';
 import image from './images/moun.png';
+import image2 from './images/bck.jpg';
 console.log('xddd');
 // console.log(x);
