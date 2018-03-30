883bbe089ddc0cff72223e6e3591281c3698ebcf
Piotrek <piotr.michalski2000@gmail.com>
2018-03-30 16:57:09 +0200

    animations

--- src/scripts.js
+++ src/scripts.js
@@ -1,69 +1,105 @@
 (function() {
+
+        // Menu scroll effect
+        window.addEventListener('scroll', () => {
+
+            const header = document.querySelector('.header'),
+                positionY = window.pageYOffset || document.documentElement.scrollTop;
+
+            if (positionY > 250) {
+                header.classList.add('opacity');
+            } else {
+                header.classList.remove('opacity');
+            }
+
+        });
+
+        // Second section container -> width++
+        window.addEventListener('scroll', () => {
+
+            const section = document.querySelector('.section__about');
+            const offset = window.pageYOffset;
+            const max_width = 1250;
+            let x = section.offsetWidth;
+
+            if (window.innerWidth >= 1440) {
+
+                if (offset > 100) {
+                    section.style.width = x + 'px';
+                    x = x + 100;
+                } else {
+                    return
+                }
+
+                if (x >= 1250) {
+                    section.style.width = max_width + 'px';
+                }
+            }
+        });
+
+        // Hamburger menu
+        const button = document.querySelector('.header__button');
+        button.addEventListener('click', (event) => {
+
+            (event ? event : event = window.event);
+            event.preventDefault();
+
+            button.classList.toggle('header__button--close');
+            document.querySelector('.nav__list').classList.toggle('nav__list--block');
+
+        });
+
+        // Detect when an element is in the viewport
+        /*const isInViewport = function ( elem ) {
+            const bounce = elem.getBoundingClientRect();
+            return (
+                bounce.top >= 0 &&
+                bounce.left >= 0 &&
+                bounce.bottom <= ( window.innerHeight || document.documentElement.clientHeight ) &&
+                bounce.right <= ( window.innerWidth || document.documentElement.clientWidth )
+            );
+        };
     
-    // Menu scroll effect
-    window.addEventListener( 'scroll', () => {
+        const item = document.querySelector( '.left' );
+        const items = Array.from( document.querySelectorAll( '.left' ) );
+        items.forEach = function() {
 
-        const header = document.querySelector( '.header' ),
-              positionY = window.pageYOffset || document.documentElement.scrollTop;
-       
-        if( positionY > 250 ) {
-            header.classList.add( 'opacity' );
-        } else {
-            header.classList.remove( 'opacity' );
-        }
-        
-    } );
-
-    // Second section container -> width++
-    window.addEventListener( 'scroll', () => {
-
-        const section = document.querySelector( '.section__about' );
-        const offset = window.pageYOffset;
-        const max_width = 1250;
-        let x = section.offsetWidth;
-
-        if ( window.innerWidth >= 1440 ) {
-        
-            if( offset > 100 ) {
-                section.style.width = x + 'px';
-                x = x + 100;
-            } else {return}
-            
-            if ( x >= 1250 ) {
-                section.style.width = max_width + 'px';
+            if ( isInViewport( items ) ) {
+                items.classList.add( 'fadeInLeft' )
+    
             } 
-            console.log(x);
-        } 
-    } );
-    
-    // Hamburger menu
-    const button = document.querySelector( '.header__button' );
-    button.addEventListener( 'click', ( event ) => {
 
-        (event ? event : event=window.event);
-        event.preventDefault();
+        }*/
+        const images_left = document.querySelectorAll('.left');
+        const images_right = document.querySelectorAll('.right');
 
-        button.classList.toggle('header__button--close');
-        document.querySelector( '.nav__list' ).classList.toggle( 'nav__list--block' );
-       
-    } );
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
 
-    // Detect when an element is in the viewport
-    const isInViewport = function ( element ) {
-        let bounce = element.getBoundingClientRect();
-        return (
-            bounce.top >= 0 &&
-            bounce.left >= 0 &&
-            bounce.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            bounce.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    };
-    
-    const element  = document.querySelector( '.section__about' );
-    
-    if ( isInViewport( element ) ) {
-        console.log( 'In the viewport!' );
-    }
+                if (entry.intersectionRatio > 0 && entry.className == "left") {
+                    entry.target.classList.add('fadeInLeft');
+                }
+
+                /*if (entry.intersectionRatio > 0 && entry.className == "right") {
+                    entry.target.classList.add('fadeInRight');
+                }*/
+
+            });
+        });
+
+        images_left.forEach(image => {
+            observer.observe(image);
+        })
+        images_right.forEach(image => {
+            observer.observe(image);
+        })
 
 
 }())
+
+
+
+
+
+
+
